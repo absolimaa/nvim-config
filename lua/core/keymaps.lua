d904ce952f6701d5116ce0197e71f8ca5c975d14
abdallahsoliman00 <abdallahsoliman03@gmail.com>
2026-01-31 00:59:23 +0200

    Changes

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,6 +4,9 @@ local opts = { noremap = true, silent = true }
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Toggle line wrapping
+vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+
 -- Default shell
 vim.opt.shell = "powershell"
 vim.opt.shellcmdflag = "-command"
@@ -22,6 +25,12 @@ vim.keymap.set('n', '<C-Down>', ':resize -2<CR>', opts)
 vim.keymap.set('n', '<C-Left>', ':vertical resize +2<CR>', opts)
 vim.keymap.set('n', '<C-Right>', ':vertical resize -2<CR>', opts)
 
+-- Navigate between window splits
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+
 -- Paste buffer override
 vim.keymap.set('v', 'p', '"_dP', opts)
 
@@ -32,7 +41,7 @@ vim.keymap.set('n', 'dd', '"_dd', opts)
 -- Buffers
 vim.keymap.set('n', '<C-Tab>', ':bnext<CR>', opts)
 vim.keymap.set('n', '<C-S-Tab>', ':bprevious<CR>', opts)
-vim.keymap.set('n', '<leader>x', ':bdelete<CR>', opts) -- close buffer
+vim.keymap.set('n', '<leader>x', ':bp | bd #<CR>', opts) -- close buffer without closing window
 vim.keymap.set('n', '<leader>t', '<cmd> enew <CR>', opts) -- new buffer
 
 -- Undo and Redo with Ctrl+Z and Ctrl+Y in insert mode
@@ -44,6 +53,12 @@ vim.keymap.set('i', '<C-Del>', '<C-o>dw', { noremap = true, silent = true })
 
 -- Git add current buffer/file
 vim.keymap.set('n', '<leader>ga', '<cmd>!git add %<CR>')
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Make Enter key add a new line
+vim.keymap.set('n', '<CR>', 'o<Esc>', { desc = 'Add newline below in normal mode' })
 
 -- Zoom in/out in Neovide
 if vim.g.neovide then
