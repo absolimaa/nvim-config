ef16d74a57619137b198a9129b4b97e40e4443f0
abdallahsoliman00 <abdallahsoliman03@gmail.com>
2026-02-21 12:31:47 +0200

    Fixed treesitter config

--- lua/plugins/plugin_files/treesitter.lua
+++ lua/plugins/plugin_files/treesitter.lua
@@ -1,14 +1,15 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate',
-  opts = {
-    ensure_installed = {
-        'c', 'cpp', 'python', 'html', 'css', 'lua',
-        'make', 'cmake', 'yaml', 'json', 'markdown',
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    config = function()
+        require('nvim-treesitter').install({
+            'c', 'cpp', 'python', 'html', 'css', 'lua',
+            'make', 'cmake', 'yaml', 'json', 'markdown',
+        })
+    end,
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
     },
-    auto_install = true,
-    highlight = { enable = true, },
-    indent = { enable = true, },
-  },
 }
